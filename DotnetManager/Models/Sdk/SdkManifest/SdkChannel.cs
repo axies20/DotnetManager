629e0a104d84d795c456badb9db33e7526eade3d
axies <maksymnikitiuk2014@gmail.com>
2026-09-17 22:11:09 +0300

    Simplify `SdkChannel` record definition

--- DotnetManager/Models/Sdk/SdkManifest/SdkChannel.cs
+++ DotnetManager/Models/Sdk/SdkManifest/SdkChannel.cs
@@ -1,16 +1,3 @@
 namespace DotnetManager.Models.Sdk.SdkManifest;
 
-public sealed record SdkChannel
-{
-    public string ChannelVersion { get; init; }
-    public string LatestSdk { get; init; }
-    public string SupportPhase { get; init; }
-    public Uri ReleasesUri { get; init; }
-
-    public SdkChannel(string ChannelVersion, string LatestSdk, string SupportPhase, Uri ReleasesUri)
-    {
-       
-    }
-
-
-}
+public sealed record SdkChannel(string ChannelVersion, string LatestSdk, string SupportPhase, Uri ReleasesUri);
