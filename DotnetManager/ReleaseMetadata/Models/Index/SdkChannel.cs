b2d8c95047e98a9fabef1eb765ca76ab9bacbd01
axies <maksymnikitiuk2014@gmail.com>
2026-09-21 03:02:28 +0300

    Add `LatestRuntime` and `LatestRelease` properties to models and adjust `ReleaseTypes` in `SdkChannel` to use `init`

--- DotnetManager/ReleaseMetadata/Models/Index/SdkChannel.cs
+++ DotnetManager/ReleaseMetadata/Models/Index/SdkChannel.cs
@@ -7,7 +7,8 @@ public sealed record SdkChannel
     public required NuGetVersion ChannelVersion { get; init; }
     public required bool Security { get; init; }
     public required string LatestSdk { get; init; }
+    public required string LatestRuntime { get; init; }
     public required SupportPhases SupportPhase { get; init; }
-    public required ReleaseTypes ReleaseTypes { get; set; }
+    public required ReleaseTypes ReleaseTypes { get; init; }
     public required Uri ReleasesUri { get; init; }
 }
--- DotnetManager/ReleaseMetadata/Models/Releases/SdkReleaseManifest.cs
+++ DotnetManager/ReleaseMetadata/Models/Releases/SdkReleaseManifest.cs
@@ -5,8 +5,16 @@ namespace DotnetManager.ReleaseMetadata.Models.Releases;
 public sealed record SdkReleaseManifest
 {
     public required NuGetVersion ChannelVersion { get; init; }
+
     public required NuGetVersion LatestSdk { get; init; }
+
+    public required NuGetVersion LatestRelease { get; init; }
+
+    public required NuGetVersion LatestRuntime { get; init; }
+
     public required SupportPhases SupportPhase { get; init; }
+
     public required ReleaseTypes ReleaseType { get; init; }
+
     public required IReadOnlyCollection<SdkRelease> Releases { get; init; }
 }
